fd1e33d1f86bbca1d1c31b6beb9cc04698f5bd65
James Hart <jhart@hellobrightline.com>
2022-10-21 15:27:05 -0700

    Add in jq and fish config

---
+++ config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+source /opt/homebrew/opt/asdf/asdf.fish
+
+set -x DANGER_GITHUB_API_TOKEN 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
+echo 'set danger thingy'
