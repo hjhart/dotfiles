f5e5dee9d94b9290365a921a089a845d03150e91
James Hart <jhart@hellobrightline.com>
2023-10-24 13:56:12 -0700

    updates from old laptop

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,7 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-source /opt/homebrew/opt/asdf/asdf.fish
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
 
 set -x DANGER_GITHUB_API_TOKEN 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
-echo 'set danger thingy'
